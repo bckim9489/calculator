d01b470cf1f8b622145897a69a5874ab0e1f2ca9
bckim9489 <bckim9489@gmail.com>
2019-04-16 06:21:50 +0000

    javadoc

--- src/main/java/com/bckim9489/calculator/Calculator.java
+++ src/main/java/com/bckim9489/calculator/Calculator.java
@@ -1,7 +1,9 @@
 package com.bckim9489.calculator;
 
 import org.springframework.stereotype.Service;
-
+/** javadoc
+	@author goood
+*/
 @Service
 public class Calculator {	
 	int sum (int a, int b){
--- src/main/java/com/bckim9489/calculator/CalculatorApplication.java
+++ src/main/java/com/bckim9489/calculator/CalculatorApplication.java
@@ -2,7 +2,9 @@ package com.bckim9489.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/** javadoc
+	@author bckim
+*/
 @SpringBootApplication
 public class CalculatorApplication {
 	public static void main(String[] args) {
